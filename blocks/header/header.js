b10f1c53d92b976a58a8bbc28651de720bc7fdc5
Katie Sexton <ksexton@adobe.com>
2022-07-14 18:19:40 -0600

    feat: overhaul header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -4,24 +4,41 @@ import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
  * collapses all open nav sections
  * @param {Element} sections The container element
  */
-
 function collapseAllNavSections(sections) {
   sections.querySelectorAll('.nav-sections > ul > li').forEach((section) => {
     section.setAttribute('aria-expanded', 'false');
   });
 }
 
+function wrapImgsInLinks(block) {
+  const pictures = block.querySelectorAll('p picture');
+  pictures.forEach((pic) => {
+    const parent = pic.parentNode;
+    const link = parent.nextElementSibling.querySelector('a');
+    if (link) {
+      link.parentElement.remove();
+      link.innerHTML = pic.outerHTML;
+      parent.replaceWith(link);
+    }
+  });
+}
+
+function setupUser(section) {
+  const icon = section.querySelector('.icon');
+  const text = section.textContent.trim();
+  section.innerHTML = `${icon.outerHTML}<span>${text}</span>`;
+}
+
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
-
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
+  const config = readBlockConfig(block);
   block.textContent = '';
 
   // fetch nav content
-  const navPath = cfg.nav || '/nav';
+  const navPath = config.nav || './nav';
   const resp = await fetch(`${navPath}.plain.html`);
   if (resp.ok) {
     const html = await resp.text();
@@ -29,9 +46,8 @@ export default async function decorate(block) {
     // decorate nav DOM
     const nav = document.createElement('nav');
     nav.innerHTML = html;
-    decorateIcons(nav);
 
-    const classes = ['brand', 'sections', 'tools'];
+    const classes = ['brand', 'sections', 'social', 'tour', 'user'];
     classes.forEach((e, j) => {
       const section = nav.children[j];
       if (section) section.classList.add(`nav-${e}`);
@@ -40,7 +56,12 @@ export default async function decorate(block) {
     const navSections = [...nav.children][1];
     if (navSections) {
       navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+        if (navSection.querySelector('ul')) {
+          navSection.classList.add('nav-drop');
+          const ul = navSection.querySelector('ul');
+          const title = navSection.innerHTML.split('\n')[0];
+          navSection.innerHTML = `<span>${title}</span>${ul.outerHTML}`;
+        }
         navSection.addEventListener('click', () => {
           const expanded = navSection.getAttribute('aria-expanded') === 'true';
           collapseAllNavSections(navSections);
@@ -60,6 +81,9 @@ export default async function decorate(block) {
     });
     nav.prepend(hamburger);
     nav.setAttribute('aria-expanded', 'false');
+
+    wrapImgsInLinks(nav);
+    setupUser(nav.querySelector('.nav-user'));
     decorateIcons(nav);
     block.append(nav);
   }
